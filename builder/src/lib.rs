a6f139982b8bd60135091e3d5e8b2ca684243bcf
Filip Laurentiu <f.laurentiu26@gmail.com>
2023-05-29 12:36:49 +0300

    Pass 02-create-builder.rs test

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,49 @@
-use proc_macro::TokenStream;
+use proc_macro::{TokenStream};
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, DeriveInput};
+use syn::Data::Struct;
+
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    let name = input.ident;
+    let builder_name = format_ident!("{}Builder", name);
+    let fields;
+
+    match input.data {
+        Struct(data_struct) => {
+            fields = data_struct.fields;
+        }
+        _ => panic!("Support only struct")
+    }
+
+    let fields_idents = fields.iter().filter_map(|field| {
+        if field.ident.is_some() {
+            Some(&field.ident)
+        } else {
+            None
+        }
+    }).collect::<Vec<_>>();
+    let fields_types = fields.iter().map(|field| &field.ty).collect::<Vec<_>>();
+
+    let res = quote! {
+        pub struct #builder_name {
+                 #(#fields_idents : std::option::Option::<#fields_types>,)*
+        }
+
+
+        impl #name {
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                    #(#fields_idents: std::option::Option::None,)*
+                }
+            }
+        }
+
+
+    };
+
+    res.into()
 }
